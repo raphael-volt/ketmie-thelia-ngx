a2d4047eecde92b1412d4f637989bd194d76c472
raphael-volt <raphael@ketmie.com>
2018-02-11 16:44:08 +0100

    added template directive

--- ngx-client/src/app/templates/templates.module.ts
+++ ngx-client/src/app/templates/templates.module.ts
@@ -2,20 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from "@angular/flex-layout";
 
-import { CategoryComponent } from './category/category.component';
 import { ApiImageDirective } from './api-image.directive';
 import { ImgTileService } from "./img-tile.service";
-import { KetmieBackgroundComponent } from './ketmie-background/ketmie-background.component';
 import { MatModule } from "../mat/mat.module";
-import { CmsContentComponent } from './cms-content/cms-content.component';
-import { ProductComponent } from './product/product.component';
 import { RoutesModule } from "../routes/routes.module";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { KetmieBackgroundComponent } from './ketmie-background/ketmie-background.component';
+import { CmsContentComponent } from './cms-content/cms-content.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
+import { ImRowComponent } from './im-row/im-row.component';
+import { ThemeComponent } from './theme/theme.component';
+
+import { SquareBoxDirective } from './square-box.directive';
 import { InnerHtmlDirective } from './inner-html.directive';
 import { SliderDirective } from "./slider.directive";
 import { ImgBoxDirective } from './product/img-box.directive';
-import { ThemeComponent } from './theme/theme.component';
+import { TemplateDirective } from './template.directive';
 
 @NgModule({
   imports: [
@@ -35,12 +39,16 @@ import { ThemeComponent } from './theme/theme.component';
     CmsContentComponent, 
     ProductComponent, 
     ImgBoxDirective, 
-    ThemeComponent
+    ThemeComponent, 
+    ImRowComponent, 
+    SquareBoxDirective, TemplateDirective
   ],
   exports:[
     KetmieBackgroundComponent,
+    ImRowComponent,
     SliderDirective,
-    ImgBoxDirective
+    ImgBoxDirective,
+    SquareBoxDirective
   ],
   providers: [ImgTileService]
 })

0b478b15ab450830d514b37edfbeb4d7e3a54a95
raphael-volt <raphael@ketmie.com>
2018-02-20 18:06:13 +0100

    use popup-service

--- ngx-client/src/app/routes/customer-guard.ts
+++ ngx-client/src/app/routes/customer-guard.ts
@@ -3,15 +3,14 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angul
 import { Observable, Observer, Subscription } from "rxjs";
 import { map } from "rxjs/operators";
 import { CustomerService } from "../api/customer.service";
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { ConnectionFormComponent } from "../templates/customer/connection-form/connection-form.component"
-import { defaultConfig } from "../mat/mat.module";
+import { PopupService } from "../popup.service";
 @Injectable()
 export class CustomerGuard implements CanActivate {
 
     constructor(
         private service: CustomerService,
-        private dialog: MatDialog) { }
+        private dialog: PopupService) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
         const service = this.service
@@ -21,9 +20,8 @@ export class CustomerGuard implements CanActivate {
 
         return Observable.create(o => {
             const addCustomerPopup = () => {
-                let ref: MatDialogRef<ConnectionFormComponent> = this.dialog.open(ConnectionFormComponent, defaultConfig)
-    
-                let sub = ref.afterClosed().subscribe((result: Observer<any>) => {
+                
+                let sub = this.dialog.afterClose(ConnectionFormComponent).subscribe((result: Observer<any>) => {
                     sub.unsubscribe()
                     o.next(service.logedIn)
                     o.complete()
